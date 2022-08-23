f2fba4df10e22ef8f6e040ae7b3fdde3204b8f6b
Javier Munhó <javiermunho@gmail.com>
2022-08-23 00:52:27 -0300

    conexion con el front
    se genero la conexion con netbeans CON LA ENTIDAD PERSONA

--- portMunho/backend/njm/src/main/java/com/portfolioMunho/njm/Controller/PersonaController.java
+++ portMunho/backend/njm/src/main/java/com/portfolioMunho/njm/Controller/PersonaController.java
@@ -5,6 +5,7 @@ import com.portfolioMunho.njm.Entity.Persona;
 import com.portfolioMunho.njm.Interface.IPersonaService;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,6 +16,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin(origins = "http//localhost4200")
 public class PersonaController {
    @Autowired IPersonaService ipersonaService;
    
@@ -45,7 +47,9 @@ public class PersonaController {
   
   ipersonaService.savePersona(persona);
   return persona;
-          
-  }
-            
+ }
+     @GetMapping("/personas/traer/perfil")
+     public Persona findPersona(){
+         return ipersonaService.findPersona((long)1);
+     }
 }
